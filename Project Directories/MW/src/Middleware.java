b0f8631ff10c6c0e5f04a08d7440ab51dc650829
Temitope Alabi <Tee@Temitopes-MacBook-Pro.local>
2018-11-25 20:02:52 -0500

    MiddleWare Package commented

--- Project Directories/MW/src/Middleware.java
+++ Project Directories/MW/src/Middleware.java
@@ -21,13 +21,17 @@ public class Middleware {
 	 * @throws TransformerException
 	 */
 	public void merge2() throws SAXException, IOException, ParserConfigurationException, TransformerException {
+		//point variable "dir" to the directory of POs
 		File dir = new File("POs/");
+		//Array rootfiles[] now containes the files in the directory of POs
 		File[] rootFiles = dir.listFiles();
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 		DocumentBuilder db = dbf.newDocumentBuilder();
 		Document doc = null;
 		String myname = "";
+		//Making an Instance of the consolidated purchase class
 		PurchCons myFinal = PurchCons.getInstance();
+		//Emptys the Map attribute of PurchCons class to avois reconsolidation of previously done POs
 		myFinal.empty();
 
 		// for (File f : rootFiles) {
@@ -37,10 +41,10 @@ public class Middleware {
 		/**
 		 * 
 		 */
-
+		//For each file in our POs directory
 		for (File f : rootFiles) {
+			//If the PO has not yet been consolidated
 			if (!(f.getAbsolutePath().contains("_done.xml"))) {
-
 				doc = db.parse(f.getAbsolutePath());
 				// System.out.println(rootFiles[i].getAbsolutePath());
 
@@ -62,27 +66,27 @@ public class Middleware {
 							}
 							// if the curr child node has the name "quantity"
 							if (myItems.item(k).getChildNodes().item(j).getNodeName().equals("quantity")) {
-								// System.out.println(myname);
-								// System.out.println(item);
-								// System.out.println("In map " +
-								// myItems.item(k).getChildNodes().item(j).getTextContent());
-								// this adds the curr quantity to the total quant in the map
+								// this adds the curr quantity of item to the total quantity of item in the map 
 								myFinal.addQuantity(item, myname,
 										Integer.parseInt(myItems.item(k).getChildNodes().item(j).getTextContent()));
+								//empty name so that for next run, it can have the name of next item
 								myname = new String("");
 							}
 
-						} else {
+						}
+						// If this is the first time it is seeing this item.
+						else {
+							
+							//when you see the tag "name"
 							if (myItems.item(k).getChildNodes().item(j).getNodeName().equals("name")) {
+								//store it's text content in variable "myname"
 								myname = new String(myItems.item(k).getChildNodes().item(j).getTextContent());
 							}
-							// If this is the first time it is seeing this item.
+							
 
 							if (myItems.item(k).getChildNodes().item(j).getNodeName().equals("quantity")) {
-								// System.out.println(item);
-								// System.out.println("outside map " +
-								// myItems.item(k).getChildNodes().item(j).getTextContent());
-								// Add the curr item num and its quan to the map
+								
+								// Add the current item number and its quantity to the map
 								myFinal.additemNumber(item, myname,
 										Integer.parseInt(myItems.item(k).getChildNodes().item(j).getTextContent()));
 								myname = new String("");
@@ -95,18 +99,22 @@ public class Middleware {
 
 			}
 		}
+		//Calls the method in Purchcons class that writes Consolidated file
 		myFinal.write();
+		
 
 	}
 
 	public void cleanDirectory() {
+		//Called after files have been consolidated in the main method
 		File dir = new File("POs/");
 		File[] rootFiles = dir.listFiles();
 
-		for (File f : rootFiles) // move consolidated P/O files to Done directory
+		for (File f : rootFiles) //For each file
 		{
+			//If it has not yet been marked as done
 			if (!f.getAbsolutePath().contains("_done.xml")) {
-
+				//mark already consolidated POs by adding "_done" to name of file
 				f.renameTo(new File(f.getAbsolutePath().replace(".xml", "_done.xml")));
 			}
 
--- Project Directories/MW/src/PurchCons.java
+++ Project Directories/MW/src/PurchCons.java
@@ -10,7 +10,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PurchCons {
-	//private Map <String, Integer> myMap = new HashMap<String, Integer>();
+	//Map object to hold the itemNumber and a list holding corresponding name & quantity
 	private Map <String,ArrayList<Object>> myMap = new HashMap<>();
 
 	private static PurchCons instance = null;
@@ -18,7 +18,7 @@ public class PurchCons {
 	public PurchCons() {
 		
 	}
-	
+	//Singleton instance
 	public synchronized static PurchCons getInstance() {
 		if(instance == null) {
 			instance = new PurchCons();
@@ -26,25 +26,26 @@ public class PurchCons {
 		
 		return instance;
 	}
-	
+	//Method to add item details to map
 	public void additemNumber(String itemNumber, String name, int quantity) {
 		ArrayList<Object> dets = new ArrayList<Object>();
+		//The dets arraylist holds the name of the item and the quantity of the item
 		dets.add(0, name);
 		dets.add(1, quantity);
-
+		//Add the itemNumber and the list holding name and quantity to the map for later extraction
 		this.myMap.put(itemNumber, dets);
-		//dets.clear();
 	}
-	/*
-	public void setitemName(String given) {
-		name = new String(given);
-	}
-	*/
+	
+	//This method sums of the quantities for all items of the same itemNumber
 	public void addQuantity(String itemNumber, String name, int quantity) {
+		//If the Map has the item
 		if(myMap.containsKey(itemNumber)) {
 			ArrayList<Object> temp = new ArrayList<Object>();
+			//Store the name in a temporary list caled temp at pos 0
 			temp.add(0, myMap.get(itemNumber).get(0));
+			//Store the total quantity of item(previous quantity + new passed quantity) in a temporary list caled temp at pos 1
 			temp.add(1, ((Integer)(myMap.get(itemNumber).get(1))+quantity));
+			//replace the itemNumber values with the new ones(as quantity has increased)
 			myMap.put(itemNumber,temp);
 		}
 	
@@ -53,50 +54,44 @@ public class PurchCons {
 	
 	public void empty()
 	{
+		//Empty my map of consolidated POs
 		myMap.clear();
 	}
+	
+	//Check if the Map already has the itemNumber
 	public boolean containsItem(String itemNumber) {
 		boolean myRes = myMap.containsKey(itemNumber);
+		//return "true" is the item already exists in map, "false" otherwise
 		return myRes;
 	}
 
-	/*
-	@Override
-	public String toString() 
-	{
-		
-		for (String name: myMap.keySet())
-		{
 
-            String key =name.toString();
-            String valuename = myMap.get(name).get(0).toString();  
-            String valuequant = myMap.get(name).get(1).toString();
-           // System.out.println(key + " " + valuename + " " + valuequant);
-            System.out.print(objectToString());
-		} 
-		return "PurchCons [myMap=" + myMap + "]";
-		
-	}
-	*/
-	
+	//Method to write Consolidated xml file for B2B
 	public void write()
 	{
+		//Get current date when files are being consolidater
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 		Date date = new Date();
 		int count = myMap.size();
 		ArrayList<String> a = new ArrayList<>();
+		//store all itemNumber in a list called "a"
 		a.addAll(myMap.keySet());
+		//formatting output to have the current date of consolidated file as opening tag
 		String myoutput = "<order submitted=\""+ dateFormat.format(date).toString() + "\">";
 		try
 		{
 		for(int i = 0; i< count; i++)
 		{
+			//writing consolidated file output
 			myoutput = myoutput + "<item number=\"" + a.get(i) + "\"><name>" + myMap.get(a.get(i)).get(0) + "</name><quantity>"+ myMap.get(a.get(i)).get(1)+"</quantity></item>";
 		}
 		myoutput= myoutput + "</order>";
+		//get Writer to file called "POtot.xml"
 		Writer output = new BufferedWriter(new FileWriter("DONE/POtot.xml"));
         String xmlOutput = myoutput; 
+      //Write the consolidated xml to file
         output.write(xmlOutput);
+        //Close the buffer
         output.close();
 		
 		}
